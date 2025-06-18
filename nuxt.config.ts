e523b969a3d3e1a44f6452504e0bb7d17a2243ec
Obi Uche <ucheobikingsley@gmail.com>
2025-06-18 10:16:15 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  routeRules: {
+    "/": {
+      ssr: false,
+    },
+  },
   devtools: { enabled: true },
   imports: {
     autoImport: true,
